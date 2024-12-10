eeed1814102f1eb4c745c22ec9b64afa78ec724b
oat <asingleoat@gmail.com>
2024-12-10 16:28:57 -0700

    repeated messages on one connection

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,50 @@ const websocket = @import("websocket");
 const Conn = websocket.Conn;
 const Message = websocket.Message;
 const Handshake = websocket.Handshake;
+const Server = websocket.Server;
 
 // Define a struct for "global" data passed into your websocket handler
 // This is whatever you want. You pass it to `listen` and the library will
 // pass it back to your handler's `init`. For simple cases, this could be empty
 const Context = struct {};
+
+fn sendMultiples(conn: *Conn) !void {
+    var buffer: [128]u8 = undefined;
+    var stream = std.io.fixedBufferStream(buffer[0..]);
+    const writer = stream.writer();
+
+    var counter: u32 = 0;
+    while (true) {
+        // Delay for 1 second
+        std.time.sleep(1_000_000_000);
+
+        // Send update to all clients
+        if (!conn.closed) {
+            std.debug.print("looping: {d}\n", .{counter});
+
+            stream.reset();
+
+            try std.fmt.format(writer, "Custom format: {d}", .{counter});
+            const result = buffer[0..stream.pos];
+
+            try conn.writeText(result);
+        }
+        counter += 1;
+    }
+}
+
+// Handler for client connections
+fn connectionHandler(conn: websocket.Connection, allocator: *const std.mem.Allocator) !void {
+    defer conn.close();
+
+    while (true) {
+        const message = try conn.readMessage(allocator);
+        defer allocator.free(message.data);
+
+        std.debug.print("Received: {s}\n", .{message.data});
+        if (message.opcode == websocket.Opcode.Close) break;
+    }
+}
 
 pub fn main() !void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
@@ -15,6 +54,19 @@ pub fn main() !void {
 
     // this is the instance of your "global" struct to pass into your handlers
     var context = Context{};
+
+    const config = .{
+        // .handshake_timeout_ms = null,
+        .port = 9223,
+        .max_headers = 10,
+        .address = "127.0.0.1",
+    };
+
+    var server = try Server.init(allocator, config);
+    defer server.deinit(allocator);
+
+    // List to store active connections
+    // var connections = std.ArrayList(websocket.Conn).init(allocator);
 
     try websocket.listen(Handler, allocator, &context, .{
         .port = 9223,
@@ -41,15 +93,17 @@ const Handler = struct {
     }
 
     // optional hook that, if present, will be called after initialization is complete
-    // pub fn afterInit(self: *Handler) !void {}
+    pub fn afterInit(self: *Handler) !void {
+        try sendMultiples(self.conn);
+    }
 
     pub fn handle(self: *Handler, message: Message) !void {
-        const my_message = "hello";
+        // const my_message[] = [127];
+        const my_message: [3]u8 = [_]u8{ 1, 2, 3 };
 
         const data = message.data;
         std.debug.print("Message: {s}\n", .{data});
-        try self.conn.writeText(my_message); // echo the message back
-
+        try self.conn.writeBin(&my_message); // echo the message back
     }
 
     // called whenever the connection is closed, can do some cleanup in here
