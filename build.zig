0ea9090a73b617c289721a1c3f7083fae3e828b5
oat <asingleoat@gmail.com>
2024-12-10 14:26:02 -0700

    websockets dependency in build.zig

--- build.zig
+++ build.zig
@@ -36,6 +36,11 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const websocket_dep = b.dependency("websocket", .{.target = target,.optimize = optimize});
+    const websocket_module = websocket_dep.module("websocket");
+
+    exe.root_module.addImport("websocket", websocket_module);
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
