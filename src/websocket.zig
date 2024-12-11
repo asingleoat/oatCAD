e8bb6c489ad9eb86112bf2584e761ad22a7a3b7f
oat <asingleoat@gmail.com>
2024-12-11 12:58:17 -0700

    factor connection handler into new module

---
+++ src/websocket.zig
@@ -0,0 +1,101 @@
+const std = @import("std");
+const websocket = @import("websocket");
+const Conn = websocket.Conn;
+const Message = websocket.Message;
+const Handshake = websocket.Handshake;
+const Server = websocket.Server;
+const model = @import("model.zig");
+
+// Define a struct for "global" data passed into your websocket handler
+// This is whatever you want. You pass it to `listen` and the library will
+// pass it back to your handler's `init`. For simple cases, this could be empty
+pub const Context = struct {};
+
+pub fn sendFixedGeometry(conn: *Conn) !void {
+    std.debug.print("sending geometry", .{});
+
+    // while (true) {
+    //     // Delay for 1 second
+    //     try conn.writeText(jsonTriangle);
+    //     std.time.sleep(16_666_667);
+    // try conn.writeText(jsonTriangle2);
+    //     std.time.sleep(16_666_667);
+    // }
+
+    const jsonTet = try model.mkJsonArray(model.unitTetSlice);
+    try conn.writeText(jsonTet);
+}
+
+pub fn sendMultiples(conn: *Conn) !void {
+    var buffer: [128]u8 = undefined;
+    var stream = std.io.fixedBufferStream(buffer[0..]);
+    const writer = stream.writer();
+
+    var counter: u32 = 0;
+    while (true) {
+        // Delay for 1 second
+        std.time.sleep(1_000_000_000);
+
+        // Send update to all clients
+        if (!conn.closed) {
+            std.debug.print("looping: {d}\n", .{counter});
+
+            stream.reset();
+
+            try std.fmt.format(writer, "Custom format: {d}", .{counter});
+            const result = buffer[0..stream.pos];
+
+            try conn.writeText(result);
+        }
+        counter += 1;
+    }
+}
+
+// Handler for client connections
+pub fn connectionHandler(conn: websocket.Connection, allocator: *const std.mem.Allocator) !void {
+    defer conn.close();
+
+    while (true) {
+        const message = try conn.readMessage(allocator);
+        defer allocator.free(message.data);
+
+        std.debug.print("Received: {s}\n", .{message.data});
+        if (message.opcode == websocket.Opcode.Close) break;
+    }
+}
+
+pub const Handler = struct {
+    conn: *Conn,
+    context: *Context,
+
+    pub fn init(h: Handshake, conn: *Conn, context: *Context) !Handler {
+        // `h` contains the initial websocket "handshake" request
+        // It can be used to apply application-specific logic to verify / allow
+        // the connection (e.g. valid url, query string parameters, or headers)
+
+        _ = h; // we're not using this in our simple case
+
+        return Handler{
+            .conn = conn,
+            .context = context,
+        };
+    }
+
+    // optional hook that, if present, will be called after initialization is complete
+    pub fn afterInit(self: *Handler) !void {
+        // try sendMultiples(self.conn);
+        try sendFixedGeometry(self.conn);
+    }
+
+    pub fn handle(self: *Handler, message: Message) !void {
+        // const my_message[] = [127];
+        const my_message: [3]u8 = [_]u8{ 1, 2, 3 };
+
+        const data = message.data;
+        std.debug.print("Message: {s}\n", .{data});
+        try self.conn.writeBin(&my_message); // echo the message back
+    }
+
+    // called whenever the connection is closed, can do some cleanup in here
+    pub fn close(_: *Handler) void {}
+};
